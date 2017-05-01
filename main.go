deadd8ad28888a671f0f1f8cb228c56f1e2632ea
Gitai <i@gitai.me>
2017-05-01 20:17:32 +0800

    string arrays to Frame struct

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"github.com/gitaiqaq/serial"
 	"io/ioutil"
+	"time"
+	"github.com/elastic/beats/libbeat/common"
 	"strings"
+	"strconv"
 	"bufio"
 	"fmt"
 	"log"
@@ -34,8 +37,21 @@ func is_Number(b byte) bool {
 	return b >= 48 && b <= 57
 }
 
-func is_Line(b []byte) bool {
+func is_Frame(b []byte) bool {
 	return is_Number(b[0]) && b[1] == 124
+}
+
+type Frame struct {
+	version 		int
+	frameType 		int
+	frameSubType 	int
+	time 		 	common.Time
+	chipId 			string
+	rssi			string
+	channel			int
+	receiverMAC		string
+	senderMAC		string
+	ssid			string
 }
 
 func main() {
@@ -47,10 +63,37 @@ func main() {
 
 		scanner := bufio.NewScanner(s.File())
 	    for scanner.Scan() {
-	    	if (is_Line(scanner.Bytes())) {
-	    		fmt.Print(scanner.Bytes())
-	    	}else{
-	    		//fmt.Println(scanner.Text())
+	    	if(is_Frame(scanner.Bytes())){
+		    	tokens := strings.Split(scanner.Text(), "|")
+		    	version, err	:= strconv.Atoi(tokens[0])
+		    	if err != nil {
+		    		continue
+		    	}
+		    	frameType, err	:= strconv.Atoi(tokens[1])
+		    	if err != nil {
+		    		continue
+		    	}
+		    	frameSubType, err	:= strconv.Atoi(tokens[2])
+		    	if err != nil {
+		    		continue
+		    	}
+		    	channel, err	:= strconv.Atoi(tokens[6])
+		    	if err != nil {
+		    		channel = 0
+		    	}
+		    	frame := Frame{
+		    		version:		version,
+					frameType:		frameType,
+					frameSubType:	frameSubType,
+					time:			common.Time(time.Now()),
+					chipId:			tokens[3],
+					rssi:			tokens[5],
+					channel:		channel,
+					receiverMAC:	tokens[7],
+					senderMAC:		tokens[8],
+					ssid:			tokens[9],
+		    	}
+		    	fmt.Println(frame)
 	    	}
 	    }
 
